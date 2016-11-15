5862b42f382c19ab1e7522cc4d855a177dd5b50b
atoms183 <sony183@bk.ru>
2016-11-15 17:37:32 +0300

    qwe

---
+++ js/main.js
@@ -0,0 +1,30 @@
+$(function(){
+   window.App ={
+        Models: {},
+		Collections: {},
+		Views:{} 
+	};
+		
+	window.template=function(id){
+	    return _.template($('#'+id).html() );
+		
+	};
+	App.Models.Task = Backbone.Model.extend({});
+	App.Views.Task = Backbone.View.extend({
+		tagName: 'li',
+		render: function (){
+			this.$el.html( this.model.get('title') );
+			return this;
+		}
+	});
+	
+	var task = new App.Models.Task({
+		title: 'Сходить в магазин',
+		priority: 4
+	});
+	var taskView = new App.Views.Task({ model: task });
+	
+	console.log(taskView.render().el);
+	
+	
+	});
